8e62bc02347e6b7cb0ef851c7de5f808895ffd0a
Mark Fine <mark.fine@gmail.com>
2017-03-30 15:30:31 -0700

    Working

--- api.rb
+++ api.rb
@@ -1,9 +1,11 @@
 require "sinatra"
 require "datadog/statsd"
 
-statsd = Datadog::Statsd.new('localhost', 8125)
+agent  = `ip route list match 0/0 | awk '{print $3}'`.chomp
+statsd = Datadog::Statsd.new(agent, 8125)
 
 get "/health" do
-  statsd.increment('dogpatch.health')
+  UDPSocket.new.send 'mfine.page.views:1|c', 0, agent, 8125
+  statsd.increment('mfine.health')
   status 200
 end
